95103f93ce97ecbee98c58cffba658314e1ed3e2
p-nikyem <92038777+p-nikyem@users.noreply.github.com>
2026-04-03 00:17:46 -0400

    validation of workload gauge component

--- smart-study-planner/src/App.js
+++ smart-study-planner/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import WorkloadGauge from './components/WorkloadGauge';
 import './App.css';
 
 function App() {
+  const assignments = [
+    {
+      subTasks: [
+        { completed: false, suggestedDate: '2026-04-03' },
+        { completed: false, suggestedDate: '2026-04-04' },
+        { completed: true, suggestedDate: '2026-04-05' },
+      ],
+    },
+    {
+      subTasks: [
+        { completed: false, suggestedDate: '2026-04-06' },
+      ],
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WorkloadGauge assignments={assignments} />
       </header>
     </div>
   );
